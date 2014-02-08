e83df14a313e63f64bb187c45044d778be8c4237
Thomas Leen <tl@thomasleen.com>
2014-02-08 16:40:54 +0000

    added bodyparser for post-style reflects and more tests

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ module.exports = function(config){
   });
 
   var app = express();
+  app.use(express.bodyParser());
+
   app.all('*',function(req, res){
     // bounce as error
   
     var meta = _.extend({date : new Date()}, _.pick(req, 'path'));
-    var response = _.extend(meta, req.query, req.body);
+    var response = _.extend({ _meta : meta}, req.query, req.body);
     res.json(response);
   });
+
   app.use(app.router);
   app.version = pkg.version;
   return app;
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var pkg = require('../package.json'),
+var moment = require('moment'),
+pkg = require('../package.json'),
 request = require('request'),
 should = require('should'),
 url = require('url'),
@@ -26,15 +27,103 @@ describe('server', function(){
   });
 
   describe('response JSON',function(){    
-    it('meta info', function(done){
-      
-      client.get(testUrl(''), function(err, response, json){
-	if(err) return done(err);
-	json.should.be.an.Object.and.have.properties('date', 'path');
-	return done();
+
+    describe('structure', function(){
+      it('should have meta info', function(done){      
+	client.get(testUrl(''), function(err, response, json){
+	  if(err) return done(err);
+	  json.should.be.an.Object.and.have.property('_meta');
+	  return done();
+	});
+      });
+
+      describe('meta info', function(){
+	it('should have properties', function(done){
+	  client.get(testUrl(''), function(err, response, json){
+	    if(err) return done(err);
+	    json._meta.should.be.an.Object.and.have.properties('date', 'path');
+	    return done();
+	  });
+	});
+
+	it('should have parsable date', function(done){
+	  client.get(testUrl(''), function(err, response, json){
+	    if(err) return done(err);
+	    json._meta.date.should.be.a.String;
+	    var m = moment(json._meta.date);
+	    m.isValid().should.be.true;
+	    return done();
+	  });
+	});
+
       });
     });
+
+    describe('returned content',function(){
+      describe('empty input', function(){	
+	it('should return just meta', function(done){      
+	  client.get(testUrl(''), function(err, response, json){
+	    if(err) return done(err);
+	    _.omit(json, '_meta').should.be.empty;
+	    return done();
+	  });
+	});
+
+	it('should have "/" as _meta.path', function(done){      
+	  client.get(testUrl(''), function(err, response, json){
+	    if(err) return done(err);
+	    json._meta.path.should.equal('/');
+	    return done();
+	  });
+	});
+      });
+
+
+      var tests = [
+	{first: 1, second : 2, third : 3},
+	{'a string' : 'a value'},
+	{'a deeper object' : {'key one' : 'value one', key2 : 2}}
+      ];
+      describe('http method',  function(){
+	describe('GET', function(){
+	  tests.forEach(function(test, i){
+	    it('should match for test #' + i, function(done){
+	      client({
+		uri : testUrl('test/get/' + i),
+		method : 'GET',
+		qs : test}, function(err, response, json){
+		  if(err) return done(err);
+		  _.omit(json,'_meta').should.eql(test);
+		  return done();
+	      });	      
+	    });
+	  });
+	});
+
+	describe('POST', function(){
+	  tests.forEach(function(test, i){
+	    it('should match for test #' + i, function(done){
+	      client({
+		uri : testUrl('test/post/' + i),
+		method : 'POST',
+		form : test}, function(err, response, json){
+		  if(err) return done(err);
+		  _.omit(json,'_meta').should.eql(test);
+		  return done();
+	      });	      
+	    });
+	  });
+	});
+      });
+
+    });
   });
+
+  describe('forced errors', function(){
+
+  });
+
+  
 
   after(function(){
     reflector.listen(4000);
